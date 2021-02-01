6bec18053a3c551721bbb1fb4f0033bc89687267
Andreas Rebsamen <andreas.rebsamen@bluewin.ch>
2021-02-01 22:03:27 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+""" 
+Audiobook player
+Designed to make playing audiobooks as comfortable as possible
+Player features inspired from: https://towardsdatascience.com/how-to-build-an-mp3-music-player-with-python-619e0c0dcee2
+"""
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
